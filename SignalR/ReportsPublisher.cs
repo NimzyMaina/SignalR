207ac8890c4ab8652e01ef8c6b9fc201ddfaa80a
Nimrod Maina <nimzy.maina@gmail.com>
2017-09-26 19:21:34 +0300

    Initial Upload

---
+++ SignalR/ReportsPublisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR
+{
+    class ReportsPublisher : Hub
+    {
+        public Task PublishReport(string reportName)
+        {
+            return Clients.All.InvokeAsync("OnReportPublished", reportName);
+        }
+    }
+}
---
+++ SignalR/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalR
+{
+    class Startup
+    {
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("fiver",
+                    policy => policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+            });
+
+            services.AddSignalR(); // <-- SignalR
+        }
+
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env)
+        {
+            app.UseCors("fiver");
+
+            app.UseSignalR(routes =>  // <-- SignalR
+            {
+                routes.MapHub<ReportsPublisher>("reportsPublisher");
+            });
+        }
+    }
+}
